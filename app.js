97d6220f0dc955e313999203024fa21390021f65
CananTyper <kispirei@gmail.com>
2026-03-12 09:29:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ function setDiscordActivity(details, state, showTimer) {
         details: details,
         state: state,
         largeImageKey: 'logo_principal', 
-        largeImageText: 'CananTyper v1.1.1 - Oficial', 
+        largeImageText: 'CananTyper v1.1.2 - Oficial', 
         instance: false,
     };
     if (showTimer) activity.startTimestamp = Date.now();
