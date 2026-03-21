344a3724b4a526bf874de97bead12c2a462311c4
CananTyper <kispirei@gmail.com>
2026-03-21 18:24:01 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -26,7 +26,6 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 window.db = firebase.firestore();
-
-window.db.enablePersistence({ synchronizeTabs: true }).catch((err) => { 
-    console.error("Persistencia falló:", err); 
+window.db.settings({
+    localCache: firebase.firestore.persistentLocalCache()
 });
